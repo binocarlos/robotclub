2e5429f56d30f2a2d6e6e9d4455d437337439879
Paul D <poleaxe@tiscali.co.uk>
2014-02-21 22:03:43 +0000

    Paul autocommit bot

---
+++ Makefile
@@ -0,0 +1,6 @@
+uploadgit:
+	git add -A
+	git commit -m "Paul autocommit bot"
+	git push
+
+.PHONY: uploadgit
---
+++ docs/spidergrams/Robotclub.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1393014641753" ID="ID_1802277288" MODIFIED="1393015957320" TEXT="Robotclub">
+<node CREATED="1393016032416" ID="ID_1490199319" MODIFIED="1393017516603" POSITION="left" TEXT="h">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1393015989679" ID="ID_1991661662" MODIFIED="1393015997710" POSITION="left" TEXT="motors"/>
+<node CREATED="1393015964505" ID="ID_197735718" MODIFIED="1393015984541" POSITION="right" TEXT="led&apos;s"/>
+</node>
+</map>
